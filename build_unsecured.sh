2ea4963ab5bed9fe917114a4ef4250074fcd264f
Wouter <wavdb66@gmail.com>
2023-01-12 14:43:25 +0000

    additional scripts additional build for debug-release

--- build_unsecured.sh
+++ build_unsecured.sh
@@ -2,9 +2,8 @@
 # 2022-06-17 16:39:31.285243
 mkdir -p build
 cd build
-cmake .. -DOC_OSCORE_ENABLED=ON
-# build step
-cmake --build .
-
+cmake .. -DOC_OSCORE_ENABLED=OFF
 # make with console
 # cmake .. -DUSE_CONSOLE=true
+# build step
+cmake --build .
