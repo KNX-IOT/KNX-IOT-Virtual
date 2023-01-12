9010082f87678f8da31c73c1b2b5cb4eb85c4513
Wouter <wavdb66@gmail.com>
2023-01-12 14:59:16 +0000

    fix in folder

--- build_unsecured.sh
+++ build_unsecured.sh
@@ -1,7 +1,7 @@
 # build script
 # 2022-06-17 16:39:31.285243
-mkdir -p build
-cd build
+mkdir -p build_unsecured
+cd build_unsecured
 cmake .. -DOC_OSCORE_ENABLED=OFF
 # make with console
 # cmake .. -DUSE_CONSOLE=true
